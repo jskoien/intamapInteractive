f2f6021686f2f7cdc9ffb897714fca1140bf265f
jskoien <jon.skoien@gmail.com>
2017-01-26 09:58:48 +0100

    correct files for intamapInteractive

--- R/common.R
+++ R/common.R
@@ -1,78 +1,87 @@
-coarsenGrid = function(object,coarse=2,offset = sample(c(0:(coarse-1)),2,replace=TRUE)) {
-objCor = as(object,"SpatialPoints")
-xss = unique(coordinates(objCor)[,1])
-yss = unique(coordinates(objCor)[,2])
-xi = c(1:(length(xss)/coarse))
-xs = sort(xss)[(xi-1)*coarse+offset[1]+1]
-yi = c(1:(length(yss)/coarse))
-ys = sort(yss)[(yi-1)*coarse+offset[2]+1]
-sel = coordinates(objCor)[,1] %in% xs & coordinates(objCor)[,2] %in% ys
-newPoints = objCor[sel,, drop = FALSE]
-if ("data" %in% names(getSlots(class(object)))) newPoints = SpatialPointsDataFrame(newPoints,data = object@data[sel,])
-#  newPointDataFrame = as(object,"SpatialPointsDataFrame")[sel,]
-gridded(newPoints) = TRUE
-newPoints
-}
 
 
 
- 
-
-
-
-commonArea = function(objecti,objectj) {
-  bi = bbox(objecti)
-  bj = bbox(objectj)
-  iarea = bbArea(bi)
-  jarea = bbArea(bj)
-  sdim = sqrt((iarea+jarea)/2)
-  bl = list()
-  for (i in 1:2) bl[[i]] =  max(bi[[i]],bj[[i]])-sdim/1000
-  for (i in 3:4) bl[[i]] =  min(bi[[i]],bj[[i]])+sdim/1000
-  if (bl[[3]] >= bl[[1]] & bl[[4]] >= bl[[2]]) {
-    larea = bbArea(bl)
-    if (larea < 0.0001*max(iarea,jarea)) larea = 0.0001*max(iarea,jarea)
-  } else {
-    larea = 0
-  }
-#
-  if (jarea< (0.0001*iarea)) jarea = 0.0001*iarea
-  if (iarea< (0.0001*jarea)) iarea = 0.0001*jarea
-  ilarea = larea/iarea
-  jlarea = larea/jarea
-
-#  print(bbox(objecti))
-#  print(bbox(objectj))
-#  print(bl)
-#  cat(paste(iarea, jarea, larea, ilarea,jlarea,"\n"))
-
-  return(list(ilarea,jlarea))
-}
-
-
-bbArea = function(bb) {
-  xd = bb[[3]]-bb[[1]]
-  yd = bb[[4]]-bb[[2]]
-  abs(xd) * abs(yd)
+ spdf2list2 = function (data) {
+# Modified function to convert a SpatialPolygonsDataFrame into a list of polygons
+# This is a new version, also checking if there are several polygons in each Polygons
+# Original function taken from GeoXp
+    if (class(data)[1] != "SpatialPolygonsDataFrame") 
+        stop("Must be a SpatialPolygonsDataFrame object")
+    poly <- data@polygons
+    n <- length(poly)
+    for (i in 1:n) {
+      np = length(poly[[i]]@Polygons)
+      for (j in 1:np) {
+        if (i ==1 & j ==1) {
+          X <- poly[1][[1]]@Polygons[[1]]@labpt[1]
+          Y <- poly[1][[1]]@Polygons[[1]]@labpt[2]
+          contours <- rbind(NA, NA, NA, poly[1][[1]]@Polygons[[1]]@coords)
+        } else {
+          X <- rbind(X, poly[i][[1]]@Polygons[[j]]@labpt[1])
+          Y <- rbind(Y, poly[i][[1]]@Polygons[[j]]@labpt[2])
+          contours = rbind(contours, NA, NA, NA, poly[i][[1]]@Polygons[[j]]@coords)
+        }
+      }
+    }
+    contours = rbind(contours, NA, NA, NA)
+    return(list(X = X, Y = Y, poly = contours))
 }
 
 
 
 
+dSolve = function(diffs) {
+  D = as.data.frame(diffs$D)
+  xnam <- paste("x", 1:dim(D)[2], sep="")
+  names(D) = xnam
+  fmla <- as.formula(paste("Q ~ ", paste(xnam, collapse= "+"),"-1"))
+  Q = diffs$Q
+  V = diffs$V
+  dat = cbind(D,Q=Q)
+  ols = as.data.frame(summary(lm(fmla,dat))$coefficients)
+  wls = as.data.frame(summary(lm(fmla,dat,weights = 1/V))$coefficients)
+  names(ols) = c("ols","ols.std","ols t value", "ols.p")
+  names(wls) = c("wls","wls.std","wls t value", "wls.p")
+  return(cbind(ols,wls))
+}
 
+#EJP: single comment sign added to all lines in this function:
+#(double comment means this was already out-commented
 
+#zip.file.extract.dir <- function (file, tmpdir, zipname = "R.zip", unzip = getOption("unzip"),zipdir)
+#{
+#    if (!is.character(unzip) || length(unzip) != 1)
+#        stop("'unzip' must be a single character string")
+#    if (!nzchar(unzip))
+#        unzip <- "internal"
+#    path <- dirname(file)
+#    topic <- basename(file)
+#    if (file.exists(file.path(path, zipname))) {
+##        if (missing(zipdir)) {
+##          tmpd <- tempdir()
+##        } else {
+##          tmpd <- zipdir
+##        }
+#        tmpd = ifelse(missing(zipdir),tempdir(),zipdir)
+#        if (unzip != "internal") {
+#            cmd <- paste(unzip, "-oq", shQuote(file.path(path,
+#                zipname)), topic, " -d ", tmpd)
+#            res <- if (.Platform$OS.type == "windows")
+#                system(cmd, invisible = TRUE)
+#            else system(paste(cmd, "> /dev/null"))
+#            if (!res)
+#                file <- file.path(tmpd, topic)
+#        }
+#        else {
+#            rc <- .Internal(int.unzip(file.path(path, zipname),
+#                topic, tmpd))
+#            if (rc == 0)
+#                file <- file.path(tmpd, topic)
+#        }
+#    }
+#    file
+#}
 
-
-SpatialDataFrame = function(spobj,data,...) {
-  if (is(spobj,"SpatialPoints")) {
-    SpatialPointsDataFrame(spobj,data = data,...)
-  } else if (is(spobj,"SpatialGrid")) {
-    SpatialGridDataFrame(spobj,data = data,...)
-  } else if (is(spobj,"SpatialPixels")) {
-    SpatialPixelsDataFrame(spobj,data = data,...)
-  } else if (is(spobj,"SpatialLines")) {
-    SpatialLinesDataFrame(spobj,data = data,...)
-  } else if (is(spobj,"SpatialPolygons")) {
-    SpatialPolygonsDataFrame(spobj,data = data,...)
-  }
+commonArea = function(...) {
+  intamap:::commonArea(...)
 }
--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-# this is needed to make R aware we're introducing new S3 methods:
-
-preProcess         <- function(object, ...) UseMethod("preProcess")
-estimateParameters <- function(object, ...) UseMethod("estimateParameters")
-spatialPredict     <- function(object, ...) UseMethod("spatialPredict")
-postProcess        <- function(object, ...) UseMethod("postProcess")
-methodParameters        <- function(object) UseMethod("methodParameters")
